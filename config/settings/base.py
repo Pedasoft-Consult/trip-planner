2c9ba98e4615b24958253e1e4f01649a4cf188c4
Pedahel Emmanuel Kojo <118267361+Pedasoft-Consult@users.noreply.github.com>
2025-09-27 18:17:05 +0000

    first files for trip planner

--- config/settings/base.py
+++ config/settings/base.py
@@ -131,8 +131,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Media files
 MEDIA_URL = '/media/'
